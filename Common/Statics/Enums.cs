99567f102c6b92181e4a2d386eac4ef4e1fd681a
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2023-02-20 17:32:18 +0100

    correct errors (left after enum type changes)

--- Common/Statics/Enums.cs
+++ Common/Statics/Enums.cs
@@ -1,15 +1,15 @@
 ﻿namespace CsabaDu.FooVar.Common.Statics;
 
 // Measures
-public enum LimitType : { BeNotLess, BeNotGreater, BeGreater, BeLess, BeEqual }
+public enum LimitType { BeNotLess, BeNotGreater, BeGreater, BeLess, BeEqual }
 
-public enum RoundingMode : { General, Ceiling, Floor, Half }
+public enum RoundingMode { General, Ceiling, Floor, Half }
 
-public enum SummingMode : { Add, Subtract }
+public enum SummingMode { Add, Subtract }
 
-public enum BaseMeasureType : { Measure, Denominator, Limit }
+public enum BaseMeasureType { Measure, Denominator, Limit }
 
 // Geometrics
-public enum Side : { Outer, Inner }
+public enum Side { Outer, Inner }
 
-public enum Comparison : { Greater, Less }
+public enum Comparison { Greater, Less }
