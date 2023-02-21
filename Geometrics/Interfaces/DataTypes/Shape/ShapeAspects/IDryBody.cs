2f1dabb7d2df5903c18aaeb1d73ba43faa0758d4
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2023-02-21 08:19:27 +0100

    Class Diagram

--- Geometrics/Interfaces/DataTypes/Shape/ShapeAspects/IDryBody.cs
+++ Geometrics/Interfaces/DataTypes/Shape/ShapeAspects/IDryBody.cs
@@ -1,18 +1,12 @@
 ﻿using CsabaDu.FooVar.Geometrics.Interfaces.Behaviors.Shape;
 using CsabaDu.FooVar.Geometrics.Interfaces.DataTypes.Spread;
 
-namespace CsabaDu.FooVar.Geometrics.Interfaces.DataTypes.Shape.ShapeAspects
+namespace CsabaDu.FooVar.Geometrics.Interfaces.DataTypes.Shape.ShapeAspects;
+
+public interface IDryBody : IBaseFace, IShape, IBody, IProjection
 {
-    public interface IDryBody : IBaseFace, IShape, IBody, IProjection
-    {
-        IExtent GetHeight();
-        IDryBody GetDryBody(ExtentUnit extentUnit);
-        IDryBody GetDryBody(params IExtent[] shapeExtents);
-        IDryBody GetDryBody(IDryBody dryBody);
-    }
-    public interface IDryBody<T> : IDryBody, IProjection<T> where T : IPlaneShape
-    {
-        T BaseFace { get; init; }
-        IExtent Height { get; init; }
-    }
+    IExtent GetHeight();
+    IDryBody GetDryBody(ExtentUnit extentUnit);
+    IDryBody GetDryBody(params IExtent[] shapeExtents);
+    IDryBody GetDryBody(IDryBody dryBody);
 }
